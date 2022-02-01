233d4d230f629b60b1931850a51f46c25487c257
Luchyn <luisdanmar@hotmail.com>
2022-01-31 21:42:55 -0300

    agregue procfile

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.get('/',(req,res) =>{
 app.use(express.static(path.join(__dirname,'./public')));
 
  
-app.listen(3000, () => {
-    console.log('servidor corriendo');
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Servidor corriendo en el puerto 3000');
 });
